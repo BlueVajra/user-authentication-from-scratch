c9ef081887c57d15eebd6374f16c1f1a1adc00a4
Cory Leistikow <cory.leistikow@gmail.com>
2014-04-17 16:12:58 -0600

    wip - working on editing users
    Signed-off-by: Cara Dennis <caracdennis@gmail.com>

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -26,6 +26,37 @@ feature 'Homepage' do
       click_on('View all users')
       expect(page).to have_content('Users')
     end
+
+    context "admin can edit users" do
+      before :each do
+        visit '/'
+        click_on('Register')
+        fill_in 'user_email', with: 'user1@test.com'
+        fill_in 'user_password', with: 'abcd'
+        fill_in 'confirm_password', with: 'abcd'
+        click_on('Register')
+        click_on('Logout')
+
+        click_on('Register')
+        fill_in 'user_email', with: 'user2@test.com'
+        fill_in 'user_password', with: 'abcd'
+        fill_in 'confirm_password', with: 'abcd'
+        click_on('Register')
+        click_on('Logout')
+      end
+      scenario "edit user" do
+        visit '/'
+        click_on('Login')
+        fill_in 'user_email', with: 'admin@gmail.com'
+        fill_in 'user_password', with: '1234'
+        click_on('Login')
+        click_on('View all users')
+        within('tr:nth-child(2)')  do
+          click_on('Edit')
+        end
+        expect(page).to have_content('user1@test.com')
+      end
+    end
   end
 
   scenario 'Shows the welcome message' do
@@ -128,4 +159,5 @@ feature 'Homepage' do
       expect(page).to have_content('Welcome user@test.com!')
     end
   end
+
 end
